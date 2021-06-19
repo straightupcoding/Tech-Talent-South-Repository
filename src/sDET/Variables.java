1f344c37ebaa37ef46062b1d4fe5e8e45ee9f189
greeneterrance92 <straightupcoding@gmail.com>
2021-06-19 17:47:09 -0500

    Submit: Java Types, Variables, & Branching
    Submit: Java Types, Variables, & Branching

---
+++ src/sDET/Variables.java
@@ -0,0 +1,16 @@
+package sDET;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		int i;
+		int j = 2;
+		byte x;
+		double w;
+		double t = 0.1;
+		
+		//int abstract continue etc. Cannot use reserved keyword
+		System.out.println(j);
+	}
+
+}
